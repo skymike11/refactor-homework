c65214fc0304317b381a28c192d3cafba2805b91
OOCLDM\lisa8 <lisa8@oocl.com>
2020-08-28 20:32:24 +0800

    [refactor-homework][sam] test: add should_return_exception_info_when_validate_employee_type_given_name_sam_and_type_teacher

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -12,3 +12,19 @@ employeeTest('should_return_user_info_when_employee_to_string_given_name_sam_and
     let expect = "sam (engineer)";
     t.is(result, expect)
 });
+
+
+employeeTest('should_return_exception_info_when_validate_employee_type_given_name_sam_and_type_teacher', t => {
+    //given
+    let name = "sam";
+    let type = "teacher";
+    let result = "";
+    try {
+        let employee = new Employee(name, type);
+    }catch (e) {
+        result = e.message;
+    }
+    //when
+    let expect = "Employee cannot be of type teacher";
+    t.is(result, expect)
+});
