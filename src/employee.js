1c3a07364dc619f9392f1ef65b822719b307ff81
OOCLDM\lisa8 <lisa8@oocl.com>
2020-08-28 20:50:35 +0800

    [refactor-homework][sam] refactor: use extend class to refactor

--- src/employee.js
+++ src/employee.js
@@ -1,25 +1,44 @@
 class Employee {
-  constructor (name, type) {
-    this.validateType(type);
+  constructor (name) {
     this._name = name;
-    this._type = type;
   }
 
-  validateType (type) {
-    if (![
-      'engineer',
-      'manager',
-      'salesman',
-    ].includes(type)) {
-      throw new Error(`Employee cannot be of type ${type}`);
-    }
+  get name(){
+    return this._name;
   }
 
   toString () {
-    return `${this._name} (${this._type})`;
+    return `${this.name} (${this.type})`;
   }
 }
 
+function createEmployee(name, type) {
+    switch (type) {
+        case 'engineer' : return new Engineer(name);
+        case 'manager' : return new Engineer(name);
+        case 'salesman' : return new Engineer(name);
+        default: throw new Error(`Employee cannot be of type ${type}`);
+    }
+}
+
+class Engineer extends Employee{
+    get type(){
+        return "engineer"
+    }
+}
+
+class Salesman extends Employee{
+    get type(){
+        return "salesman"
+    }
+}
+
+class Manager extends Employee{
+    get type(){
+        return "manager"
+    }
+}
+
 module.exports = {
-  Employee
+    createEmployee
 };
