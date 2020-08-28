0af5a7395135c3b1c9c2b4a177a6eaf984d600fc
OOCLDM\lisa8 <lisa8@oocl.com>
2020-08-28 21:06:42 +0800

    [refactor-homework][sam] test: add should_return_plusDays_5_when_deliveryDate_given_anOrder_with_ME_state_isRush_false

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -1,7 +1,7 @@
 import {deliveryDate} from "../src/delivery";
 
 const deliveryTest = require('ava');
-deliveryTest('should_return_plusDays_1_when_deliveryDate_given_anOrder_with_MA_state_isRush_true', t => {
+deliveryTest('should_return_plusDays_2_when_deliveryDate_given_anOrder_with_MA_state_isRush_true', t => {
     //given
     const isRush = true;
     const anOrder = {
@@ -15,4 +15,68 @@ deliveryTest('should_return_plusDays_1_when_deliveryDate_given_anOrder_with_MA_s
     //when
     let result = deliveryDate(anOrder, isRush);
     t.is(result, 2)
+});
+
+deliveryTest('should_return_plusDays_3_when_deliveryDate_given_anOrder_with_NY_state_isRush_true', t => {
+    //given
+    const isRush = true;
+    const anOrder = {
+        deliveryState : "NY",
+        placedOn : {
+            plusDays : (deliveryTime)=>{
+                return deliveryTime
+            }
+        }
+    };
+    //when
+    let result = deliveryDate(anOrder, isRush);
+    t.is(result, 3)
+});
+
+deliveryTest('should_return_plusDays_4_when_deliveryDate_given_anOrder_with_none_state_isRush_true', t => {
+    //given
+    const isRush = true;
+    const anOrder = {
+        deliveryState : "none",
+        placedOn : {
+            plusDays : (deliveryTime)=>{
+                return deliveryTime
+            }
+        }
+    };
+    //when
+    let result = deliveryDate(anOrder, isRush);
+    t.is(result, 4)
+});
+
+deliveryTest('should_return_plusDays_4_when_deliveryDate_given_anOrder_with_MA_state_isRush_false', t => {
+    //given
+    const isRush = false;
+    const anOrder = {
+        deliveryState : "MA",
+        placedOn : {
+            plusDays : (deliveryTime)=>{
+                return deliveryTime
+            }
+        }
+    };
+    //when
+    let result = deliveryDate(anOrder, isRush);
+    t.is(result, 4)
+});
+
+deliveryTest('should_return_plusDays_5_when_deliveryDate_given_anOrder_with_ME_state_isRush_false', t => {
+    //given
+    const isRush = false;
+    const anOrder = {
+        deliveryState : "ME",
+        placedOn : {
+            plusDays : (deliveryTime)=>{
+                return deliveryTime
+            }
+        }
+    };
+    //when
+    let result = deliveryDate(anOrder, isRush);
+    t.is(result, 5)
 });
